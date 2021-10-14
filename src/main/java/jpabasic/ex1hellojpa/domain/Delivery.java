1ccb0aaf70dbe9050d586c999df4d2a425d879f0
IcedAmeri <seokhyunkho@gmail.com>
2021-10-14 22:08:02 +0900

    [프록시와 연관관계 관리] 실전 예제 5 - 연관관계 관리

--- src/main/java/jpabasic/ex1hellojpa/domain/Delivery.java
+++ src/main/java/jpabasic/ex1hellojpa/domain/Delivery.java
@@ -1,6 +1,7 @@
 package jpabasic.ex1hellojpa.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
@@ -15,6 +16,6 @@ public class Delivery extends BaseEntity {
     private String zipcode;
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "delivery")
     private Order order;
 }
